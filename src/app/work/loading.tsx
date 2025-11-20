72f38c6b0e65e5b88ff9eef0e78430650e33f64e
EAGLE1309 <vighneshbangar57@gmail.com>
2025-11-20 17:22:20 +0530

    Testing loading state

---
+++ src/app/work/loading.tsx
@@ -0,0 +1,7 @@
+import MaxWidthWrapper from "@/components/layout/max-width-wrapper";
+
+const WorkLoading = () => {
+  return <MaxWidthWrapper className="h-[100vh] w-full overflow-hidden flex items-center justify-center gap-3">Loading...</MaxWidthWrapper>;
+};
+
+export default WorkLoading;
