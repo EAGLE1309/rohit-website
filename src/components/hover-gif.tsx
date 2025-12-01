87f75f072af7ad417c915ea5dbdc9c7600bcba10
EAGLE1309 <vighneshbangar57@gmail.com>
2025-12-02 01:47:18 +0530

    Fixes

--- src/components/hover-gif.tsx
+++ src/components/hover-gif.tsx
@@ -17,7 +17,7 @@ export function HoverGif({ staticSrc, gifSrc, alt = "", className = "" }: HoverG
   return (
     <div className={cn("relative overflow-hidden", className)} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
       <Image src={staticSrc} alt={alt} fill priority className="object-cover" sizes="(max-width: 768px) 80px, 230px" />
-      {isHovered && <img src={gifSrc} alt={alt} className="absolute inset-0 w-full h-full object-cover" />}
+      {!isHovered && <img src={gifSrc} alt={alt} className="absolute inset-0 w-full h-full object-cover" />}
     </div>
   );
 }
